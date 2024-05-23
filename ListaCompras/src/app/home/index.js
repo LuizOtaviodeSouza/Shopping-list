cdb4a5031c6184daa3b9ff64978df203875200eb
LuizOtaviodeSouza <lui-otavio@hotmail.com>
2024-05-23 11:39:29 -0300

    Demoro

--- ListaCompras/src/app/home/index.js
+++ ListaCompras/src/app/home/index.js
@@ -1,17 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
   View, 
   Text,
   SafeAreaView,
   ImageBackground,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  FlatList,
+  Alert
 } from 'react-native'
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from './style';
 import ItemList from '../../components/ItemList';
+import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
+  const [textInput, setTextInput] = useState('');
+  const [items, setItems] = useState([]);
+
+  const saveItemToDevice = async () => {
+
+  }
+
+  const getItemsFromDevice = async () => {
+
+  }
+
+  const addItem = () => {
+
+  }
+
+  const markItemBought = itemId => {
+
+  }
+
+  const unmarkItemBought = itemId => {
+
+  }
+  
+  const removeItem = itemId => {
+
+  }
+
+  const removeAll = () => {
+    Alert.alert("Limpar Lista?", 
+    "Confirmar a exclusão de todos os produtos da sua lista",
+    [{
+      text: 'Sim',
+      onPress: () => { setItems([]) }
+    }, {
+      text: "Cancelar",
+      style: "cancel",
+    }]
+    )
+  }
+
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ImageBackground
@@ -22,13 +67,22 @@ export default function Home() {
         <View style={styles.header}>
           <Text style={styles.title}>Lista de Produtos</Text>
           <View>
-            <Ionicons name="trash" size={36} color="#fff" />
+            <Ionicons name="trash" size={36} color="#fff" onPress={removeAll} />
           </View>
         </View>
 
         {/* Lista de Produtos */}
-        <ItemList item={"Café"}></ItemList>
-        <ItemList item={"Chocolate"}></ItemList>
+        <FlatList 
+          contentContainerStyle={{ padding: 20, paddingBottom: 100, color: '#fff' }}
+          data={items}
+          renderItem={({item}) =>
+            <ListItem item={item}
+            markItem ={markItemBought}
+            unmarkItem={unmarkItemBought}
+            removeItem={removeItem}
+            />
+          }
+        />
 
         <View style={styles.footer}>
           <View style={styles.TextContainer}>
@@ -37,9 +91,11 @@ export default function Home() {
            fontSize={18}
            placeholderTextColor="#fff"
            placeholder="Digite o nome do produto..."
+           value={textInput}
+           onChange={(text) => setTextInput(text)}
            />
           </View>
-          <TouchableOpacity style={styles.iconContainer}>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem}>
            <Ionicons name="add" size={36} color="#fff" />
           </TouchableOpacity>
         </View>
--- ListaCompras/src/components/ItemList/index.js
+++ ListaCompras/src/components/ItemList/index.js
@@ -6,9 +6,33 @@ import { styles } from "./style";
 export default function ItemList({ item, markItem, unmarkItem, removeItem }) {
   return (
     <View style={styles.itemList}>
-        <View style={{flex: 1}}>
-         <Text style = {styles.itemToBuy}>{item}</Text>
-        </View>
+      <View style={{flex: 1}}>
+        <Text style = {item?.bought ? styles.itemBought : styles.itemToBuy}>
+         {item?.name}
+        </Text>
+      </View>
+      {!item?.bought ? (
+        <TouchableOpacity
+         styles={styles.actionIcon}
+         onPress={() => markItem(item)}
+        >
+         <Ionicons name='bag-check-outline' size={24} color='#fff' />
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+         styles={styles.actionIcon}
+         onPress={() => unmarkItem(item)}
+        >
+          <Ionicons name='bag-remove-outline' size={24} color='#fff' />
+        </TouchableOpacity>
+      ) }
+
+        <TouchableOpacity
+         styles={[styles.actionIcon, { backgroundColor: "darkred"}]}
+         onPress={() => removeItem(item)}
+        >
+          <Ionicons name='trash-bin-outline' size={24} color='#fff' />
+        </TouchableOpacity>
     </View>
   )
 }
